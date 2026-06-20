b179a03e6ffc1a31f44e509e698a278ecc9054b3
Ayomide <ayomidesofolahan@gmail.com>
2026-06-20 12:11:42 +0100

    Add Questions table schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -8,3 +8,14 @@ target_score INTEGER,
 test_date DATE,
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE Questions(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+exam_type TEXT,
+subject TEXT,
+difficulty TEXT,
+question_text TEXT,
+options TEXT,
+correct_answer TEXT,
+explamation TEXT
+);
